91b8a44b7633d1998c874b7d40a76d50778dff2b
Brion Lund <me@brionispopt.art>
2024-03-13 21:12:45 -0700

    created game with basic functionality

---
+++ cmd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+
+	"github.com/brionispoptart/kibibytris/internal/pkg/game" // Adjusted import path
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+func main() {
+	ebiten.SetWindowSize(500, 1000)
+	ebiten.SetWindowTitle("kibibytris")
+
+	gameInstance := game.NewGame() // Corrected to reference the game package
+
+	if err := ebiten.RunGame(gameInstance); err != nil {
+		log.Fatal(err)
+	}
+}
